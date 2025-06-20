281c6987c002a0591724389f91541a9a8e36411b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-06-20 21:33:52 +0530

    Create browser extension to copy tab urls
    A simple chrome extension to instantly copy the url of the current (or selected) tabs through a keyboard shortcut.

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.commands.onCommand.addListener(cmd => {
+    if (cmd === 'copy-tab-url') {
+        chrome.tabs.query({ highlighted: true, currentWindow: true }, tabs => {
+            const urls = tabs.map(tab => tab.url).join('\n')
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: (text) => navigator.clipboard.writeText(text),
+                args: [urls]
+            })
+        })
+    }
+})
