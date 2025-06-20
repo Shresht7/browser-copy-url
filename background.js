59a380ff9e6fd8c55fed1bbc6f3541289f67cc20
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-06-20 21:35:14 +0530

    Extract copying logic out into `copyUrl` function

--- background.js
+++ background.js
@@ -1,12 +1,17 @@
 chrome.commands.onCommand.addListener(cmd => {
     if (cmd === 'copy-tab-url') {
-        chrome.tabs.query({ highlighted: true, currentWindow: true }, tabs => {
-            const urls = tabs.map(tab => tab.url).join('\n')
-            chrome.scripting.executeScript({
-                target: { tabId: tabs[0].id },
-                func: (text) => navigator.clipboard.writeText(text),
-                args: [urls]
-            })
-        })
+        copyUrl()
     }
 })
+
+function copyUrl() {
+    chrome.tabs.query({ highlighted: true, currentWindow: true }, tabs => {
+        const urls = tabs.map(tab => tab.url).join('\n')
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: (text) => navigator.clipboard.writeText(text),
+            args: [urls]
+        })
+    })
+}
+
